8d319e2ee114f8073c15c834e0d69fa0270d2962
edSantiago01 <santiago7nubesoft@gmail.com>
2015-11-28 23:14:12 -0600

    404

---
+++ 404.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <?php include ("./secciones/head.php"); ?>
+  <title>Página no encontrada | Aventura en Globos</title>
+
+</head>
+<body>
+  <?php include ("./secciones/menu.php"); ?>
+
+<div class="errorFondo contenidoErr">
+  <div class="fondo2 contenidoErr">
+
+
+  <div class="container contenidoPag">
+
+    <div class="row">
+      <div class="col-md-12">
+        <h2 class="text-warning"><i class="fa fa-exclamation-circle"></i> 404 </h2>
+          <h2>Lo sentimos, no encontramos la página a la que intentas acceder. </h2>
+        <hr>
+        <h3>Motivos:</h3>
+        <ul>
+          <li>Tal vez la página se movió, quizó vivir la experiencia de volar.</li>
+          <li>No siguió las indicaciones del capitán de vuelo y fue eliminada del sitio web.</li>
+          <li>Quizá nunca ha existido y es producto de la imaginación.</li>
+          <li>Los desarrolladores fueron por un café y aún no la publican.</li>
+        </ul>
+        <br><br>
+        <h3><i class="fa fa-hand-o-right"></i>
+          <a  href="index.php#home">
+            Puedes volver a la <strong>página principal</strong> y seguir navegando.
+          </a> </h3>
+
+      </div>
+    </div>
+
+
+  </div>
+  </div>
+</div>
+
+
+<script src="./js/jquery.js"></script>
+<script src="./js/efectos.js"></script>
+<script src="./bootstrap/js/bootstrap.min.js"></script>
+</body>
+</body>
+</html>
