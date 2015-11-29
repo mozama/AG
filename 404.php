30c059fa6b3dd7dca19d200a6322077fd8351528
edSantiago01 <santiago7nubesoft@gmail.com>
2015-11-29 11:40:38 -0600

    correcion 404

--- 404.php
+++ 404.php
@@ -1,12 +1,77 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-  <?php include ("./secciones/head.php"); ?>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="author" href="www.aventuraenglobos.com/humans.txt" />
+
+  <link href="http://www.aventuraenglobos.com/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="http://www.aventuraenglobos.com/css/font-awesome.min.css" rel="stylesheet">
+  <link href="http://www.aventuraenglobos.com/css/style.css" rel="stylesheet">
+  <link rel="icon" type="image/png" href="http://www.aventuraenglobos.com/favicon.png"/>
   <title>Página no encontrada | Aventura en Globos</title>
 
 </head>
 <body>
-  <?php include ("./secciones/menu.php"); ?>
+  <nav class="navbar navbar-default navbar-fixed-top" role="navigation" id="barraNav">
+      <div class="navbar-header" >
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <div class="contLogo">
+          <a href="index.php#home">
+            <img id="imgI" class="img-responsive" src="http://www.aventuraenglobos.com/images/globos_01.jpg">
+          </a>
+        </div>
+      </div>
+
+      <div class="collapse navbar-collapse navbar-ex1-collapse text-right">
+        <ul class="nav navbar-nav navbar-right">
+          <li id="navInicio">
+            <a  href="http://www.aventuraenglobos.com/index.php#home">Inicio</a>
+          </li>
+          <li id="navPaquete">
+            <a href="http://www.aventuraenglobos.com/vuelos.php">Paquetes</a>
+          </li>
+          <li id="navNosotros">
+            <a  href="http://www.aventuraenglobos.com/index.php#features">Nosotros</a>
+          </li>
+          <li id="navContacto">
+            <a href="http://www.aventuraenglobos.com/index.php#contact">Contacto</a>
+          </li>
+          <li id="navGaleria">
+            <a href="http://www.aventuraenglobos.com/galerias.php">Galerías</a>
+          </li>
+          <li id="navRecervacion">
+            <a href="http://www.aventuraenglobos.com/recervacion.php">Reservación</a>
+          </li>
+          <li>
+            <img id="imgI" class="img-responsive" src="images/banderag.gif">
+          </li>
+        </ul>
+      </div>
+
+      <div class="container"> <!-- Elemento ribbon que se queda fijo -->
+       <div class="row">
+           <div class="ribbon2" style="right: 0">
+               <a href="recervacion.php" class="enlaceSimpleR">
+                 <h4 style="margin: 0;padding: 5px 5px ">
+                   <strong>Reservación</strong>
+                   <br>
+                   <i class="fa fa-mobile"></i></strong> 55 51 98 63 99 <br><br>
+                   <a href="index.php#contact" class="enlaceSimpleR">Contacto</a>
+                 </h4>
+               </a>
+           </div>
+       </div>
+     </div>
+
+  </nav>
+
+
 
 <div class="errorFondo contenidoErr">
   <div class="fondo2 contenidoErr">
@@ -28,7 +93,7 @@
         </ul>
         <br><br>
         <h3><i class="fa fa-hand-o-right"></i>
-          <a  href="index.php#home">
+          <a  href="http://www.aventuraenglobos.com/">
             Puedes volver a la <strong>página principal</strong> y seguir navegando.
           </a> </h3>
 
@@ -41,9 +106,9 @@
 </div>
 
 
-<script src="./js/jquery.js"></script>
-<script src="./js/efectos.js"></script>
-<script src="./bootstrap/js/bootstrap.min.js"></script>
+<script src="http://www.aventuraenglobos.com/js/jquery.js"></script>
+<script src="http://www.aventuraenglobos.com/js/efectos.js"></script>
+<script src="http://www.aventuraenglobos.com/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </body>
 </html>
