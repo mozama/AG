d974ca44bd0182268240c2b1cb57a2d906509d27
anyer23 <monterrubio_19@hotmail.com>
2017-04-07 14:30:41 -0500

    cambio de precios en hospedaje

--- hospedaje.php
+++ hospedaje.php
@@ -41,26 +41,26 @@
             <p class="textoIndex2">Completa tu paquete con hospedaje:</p>
             <ul>
               <li class="junction">Costo por noche de hospedaje.</li>
-              <li class="junction">Habitación ocupación <span class="text-primary"> doble</span> <span class="text-warning">MX $1,100</span> pesos por noche.</li>
-              <li class="junction">Habitación ocupación <span class="text-primary"> triple</span> <span class="text-warning">MX $1,350</span> pesos por noche.</li>
-              <li class="junction">Habitación ocupación <span class="text-primary"> cuádruple</span> <span class="text-warning">MX $1,450</span> pesos por noche.</li>
+              <li class="junction">Habitación ocupación <span class="text-primary"> doble</span> <span class="text-warning">MX $1,200</span> pesos por noche.</li>
+              <li class="junction">Habitación ocupación <span class="text-primary"> triple</span> <span class="text-warning">MX $1,400</span> pesos por noche.</li>
+              <li class="junction">Habitación ocupación <span class="text-primary"> cuádruple</span> <span class="text-warning">MX $1,500</span> pesos por noche.</li>
             </ul>
             <br>
           <!-- carrito quinto sol-->
                <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-               <input type="hidden" name="cmd" value="_s-xclick">
-               <input type="hidden" name="hosted_button_id" value="TWB97YKBALNFL">
-               <table>
-               <tr><td><input type="hidden" name="on0" value="Hotel Quinto Sol Habitacion"></td></tr><tr><td><select name="os0">
-                 <option value="Habitacion ocupacion doble">Habitación ocupación doble $1,100.00 MXN</option>
-                 <option value="Habitacion ocupacion triple">Habitación ocupación triple $1,350.00 MXN</option>
-                 <option value="Habitacion ocupacion cuadruple">Habitación ocupación cuadruple $1,450.00 MXN</option>
-               </select> </td></tr>
-               </table>
-               <input type="hidden" name="currency_code" value="MXN">
-               <input type="image" src="https://www.paypalobjects.com/es_XC/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
-               <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
-               </form>
+                   <input type="hidden" name="cmd" value="_s-xclick">
+                   <input type="hidden" name="hosted_button_id" value="TWB97YKBALNFL">
+                    <table>
+                    <tr><td><input type="hidden" name="on0" value="Hotel Quinto Sol Habitacion">Hotel Quinto Sol Habitación</td></tr><tr><td><select name="os0">
+                      <option value="Habitacion ocupacion doble">Habitación ocupación doble $1,200.00 MXN</option>
+                      <option value="Habitacion ocupacion triple">Habitación ocupación triple $1,400.00 MXN</option>
+                      <option value="Habitacion ocupacion cuadruple">Habitación ocupación cuadruple $1,500.00 MXN</option>
+                    </select> </td></tr>
+                    </table>
+                   <input type="hidden" name="currency_code" value="MXN">
+                   <input type="image" src="https://www.paypalobjects.com/es_XC/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
+                    <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+                </form>
           <!--fin carrito-->
           </div>
           <div class="col-md-4">
@@ -83,20 +83,31 @@
                   <p class="textoIndex2">Completa tu paquete con hospedaje:</p>
                   <ul>
                     <li class="junction">Costo por noche de hospedaje.</li>
-                    <li class="junction">Habitación ocupación <span class="text-primary"> doble</span> <span class="text-warning">MX $1,100</span> pesos por noche.</li>
+                    <li class="junction">Habitación ocupación <span class="text-primary"> doble</span> <span class="text-warning">MX $1,200</span> pesos por noche.</li>
+                    <li class="junction">Habitación ocupación <span class="text-primary"> triple</span> <span class="text-warning">MX $1,400</span> pesos por noche.</li>
+                    <li class="junction"><span class="text-primary">Suite</span> <span class="text-warning">MX $1,800</span> pesos por noche.</li>
                   </ul>
+
                <!--carrito villas-->
                   <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-                  <input type="hidden" name="cmd" value="_s-xclick">
-                  <input type="hidden" name="hosted_button_id" value="NM22UYPJY7ANJ">
-                  <input type="image" src="https://www.paypalobjects.com/es_XC/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
-                  <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+                   <input type="hidden" name="cmd" value="_s-xclick">
+                   <input type="hidden" name="hosted_button_id" value="NM22UYPJY7ANJ">
+                       <table>
+                         <tr><td><input type="hidden" name="on0" value="Hotel Villas Arqueologicas">Hotel Villas Arqueologicas</td></tr><tr><td><select name="os0">
+                           <option value="Habitacion ocupacion doble">Habitacion ocupacion doble $1,200.00 MXN</option>
+                          <option value="Habitacion ocupacion triple">Habitacion ocupacion triple $1,400.00 MXN</option>
+                           <option value="Suite">Suite $1,800.00 MXN</option>
+                       </select> </td></tr>
+                      </table>
+                    <input type="hidden" name="currency_code" value="MXN">
+                    <input type="image" src="https://www.paypalobjects.com/es_XC/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
+                      <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
                   </form>
                <!-- fin carrito-->
                 </div>
                 <div class="col-md-4">
                     <br><br>
-                    <a class="btn btn-primary btn-block" href="http://www.villasmex.com/" target=blanck><i class="fa fa-link"></i> Ver sitio web Hotel Villas Arqueolgogicas</a>
+                    <a class="btn btn-primary btn-block" href="http://www.villasmex.com/" target=blanck><i class="fa fa-link"></i> Ver sitio web Hotel Villas Arqueologicas</a>
                 </div>
               </div>
 
@@ -114,17 +125,24 @@
                   <p class="textoIndex2">Completa tu paquete con hospedaje:</p>
                   <ul>
                     <li class="junction">Costo por noche de hospedaje.</li>
-                    <li class="junction">Habitación ocupación <span class="text-primary"> doble</span> <span class="text-warning">MX $850</span> pesos por noche.</li>
-                    <li class="junction">Jacuzzi.</li>
+                    <li class="junction">Habitación ocupación <span class="text-primary"> doble</span> <span class="text-warning">MX $750</span> pesos por noche.</li>
+                    <li class="junction">Habitación ocupación doble con <span class="text-primary">Jacuzzi</span><span class="text-warning"> MX $900</span> pesos por noche. </li>
                   </ul>
 
                   <!--carrito ollin teotl-->
-                      <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-                      <input type="hidden" name="cmd" value="_s-xclick">
-                      <input type="hidden" name="hosted_button_id" value="3T5CU2DGTCAF4">
-                      <input type="image" src="https://www.paypalobjects.com/es_XC/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
-                      <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
-                      </form>
+                  <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                    <input type="hidden" name="cmd" value="_s-xclick">
+                    <input type="hidden" name="hosted_button_id" value="3T5CU2DGTCAF4">
+                        <table>
+                            <tr><td><input type="hidden" name="on0" value="Hotel Ollit Teotl">Hotel Ollit Teotl</td></tr><tr><td><select name="os0">
+                              <option value="Habitacion ocupacion doble">Habitacion ocupacion doble $750.00 MXN</option>
+                              <option value="Habitacion ocupacion doble con jacuzzi">Habitacion ocupacion doble con jacuzzi $900.00 MXN</option>
+                            </select> </td></tr>
+                        </table>
+                    <input type="hidden" name="currency_code" value="MXN">
+                    <input type="image" src="https://www.paypalobjects.com/es_XC/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
+                       <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+                  </form>
                 <!-- fin de carrito-->
                 </div>
                 <div class="col-md-4">
@@ -147,25 +165,25 @@
                   <p class="textoIndex2">Completa tu paquete con hospedaje:</p>
                   <ul>
                     <li class="junction">Costo por noche de hospedaje.</li>
-                    <li class="junction">Habitación ocupación <span class="text-primary"> doble</span> <span class="text-warning">MX $600</span> pesos por noche.</li>
-                    <li class="junction">Habitación ocupación <span class="text-primary"> triple</span> <span class="text-warning">MX $650</span> pesos por noche.</li>
-                    <li class="junction">Habitación ocupación <span class="text-primary"> cuádruple</span> <span class="text-warning">MX $700</span> pesos por noche.</li>
+                    <li class="junction">Habitación ocupación <span class="text-primary"> doble</span> <span class="text-warning">MX $650</span> pesos por noche.</li>
+                    <li class="junction">Habitación ocupación <span class="text-primary"> triple</span> <span class="text-warning">MX $700</span> pesos por noche.</li>
+                    <li class="junction">Habitación ocupación <span class="text-primary"> cuádruple</span> <span class="text-warning">MX $800</span> pesos por noche.</li>
                   </ul>
                   <!-- carrito posada jade-->
-                          <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-                          <input type="hidden" name="cmd" value="_s-xclick">
-                          <input type="hidden" name="hosted_button_id" value="WED23N73JYXVG">
-                          <table>
-                          <tr><td><input type="hidden" name="on0" value="Posada Jade Hataciones"></td></tr><tr><td><select name="os0">
-                            <option value="Habitacion ocupacion doble">Habitación ocupación doble $600.00 MXN</option>
-                            <option value="Habitacion ocupacion tiple">Habitación ocupación tiple $650.00 MXN</option>
-                            <option value="Habitacion ocupacion Cuadruple">Habitación ocupación Cuadruple $700.00 MXN</option>
-                          </select> </td></tr>
-                          </table>
-                          <input type="hidden" name="currency_code" value="MXN">
-                          <input type="image" src="https://www.paypalobjects.com/es_XC/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
-                          <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
-                          </form>
+                  <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                      <input type="hidden" name="cmd" value="_s-xclick">
+                      <input type="hidden" name="hosted_button_id" value="WED23N73JYXVG">
+                        <table>
+                            <tr><td><input type="hidden" name="on0" value="Posada Jade Hataciones">Posada Jade Hataciones</td></tr><tr><td><select name="os0">
+                                <option value="Habitacion ocupacion doble">Habitacion ocupacion doble $650.00 MXN</option>
+                                <option value="Habitacion ocupacion tiple"> Habitacion ocupacion tiple $700.00 MXN</option>
+                                <option value="Habitacion ocupacion Cuadruple">Habitacion ocupacion Cuadruple $800.00 MXN</option>
+                            </select> </td></tr>
+                        </table>
+                      <input type="hidden" name="currency_code" value="MXN">
+                      <input type="image" src="https://www.paypalobjects.com/es_XC/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea.">
+                         <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+                  </form>
                   <!--fin carrito-->
                 </div>
                 <div class="col-md-4">
